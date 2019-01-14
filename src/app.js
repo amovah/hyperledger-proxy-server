80cdab908e252c69966fe2ebeab7e3cc887df618
Ali Movahedi <ali_movahedi@aol.com>
2019-01-14 13:27:04 +0330

    feat: fetch data from blockchain backend

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
+import 'babel-polyfill';
 import express from 'express';
+import request from 'request-promise-native';
 
 const app = express();
+const server = 'http://66.70.130.225:8080/api/blockAndTxList/1b6b205c6bfa19878a08118392de814a2a1a1df67339a05d0a31b845eb8937a7/0?'; // eslint-disable-line
+const generateUrl = (from, to) => `${server}from=${from}&&${to}`;
 
 app.use((req, res, next) => {
   res.set('Access-Control-Allow-Origin', '*');
@@ -8,8 +12,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-  res.json({});
+app.get('/', async (req, res) => {
+  const response = await request(generateUrl(new Date(0).toString(), Date().toString()));
+  res.json(JSON.parse(response));
 });
 
 app.listen(3015, () => {
