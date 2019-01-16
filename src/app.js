b7f377f187c9ed31c0ecfb3525c318ef348b7b24
Ali Movahedi <ali_movahedi@aol.com>
2019-01-16 21:53:41 +0330

    fix: generate url
    fix: fix unresponsed request

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import request from 'request-promise-native';
 
 const app = express();
 const server = 'http://66.70.130.225:8080/api/blockAndTxList/1b6b205c6bfa19878a08118392de814a2a1a1df67339a05d0a31b845eb8937a7/0?'; // eslint-disable-line
-const generateUrl = (from, to) => `${server}from=${from}&&${to}`;
+const generateUrl = (from, to) => `${server}from=${escape(from)}&&to=${escape(to)}`;
 
 app.use((req, res, next) => {
   res.set('Access-Control-Allow-Origin', '*');
@@ -13,8 +13,14 @@ app.use((req, res, next) => {
 });
 
 app.get('/', async (req, res) => {
-  const response = await request(generateUrl(new Date(0).toString(), Date().toString()));
-  res.json(JSON.parse(response));
+  try {
+    const response = await request(generateUrl(new Date(0).toString(), Date().toString()));
+    res.json(JSON.parse(response));
+  } catch (e) {
+    res.json({
+      rows: [],
+    });
+  }
 });
 
 app.listen(3015, () => {
